f5d73952e12c1e6b99e49a25142cb30f899e8cdd
catkate13 <57996108+catkate13@users.noreply.github.com>
2020-07-16 22:21:01 +0300

    Add files via upload

---
+++ HW1/source.js
@@ -0,0 +1,30 @@
+//exercise 2
+
+console.log `Riaboshapko`;
+
+//exercise 3
+let a;
+let b;
+a = 1;
+b = 2;
+document.write (a, b);
+document.write (a=b)
+
+//exercise 4
+
+let exercise4 = {
+     string: 'Lorem Ipsum', 
+     number: 7, 
+     boolean: true, 
+     test1 : undefined,
+     test2: null
+    }               
+//exercise 5
+
+let isAdult = confirm("Are you18?");
+
+console.log (isAdult)
+  
+
+
+
