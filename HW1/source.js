aa4bb410baf95bb69ae42e8ebb7a94dd0af1fc84
catkate13 <57996108+catkate13@users.noreply.github.com>
2020-07-20 03:17:24 +0300

    Add files via upload

---
+++ HW1/source.js
@@ -0,0 +1,65 @@
+//exercise 2
+
+console.log `Riaboshapko`;
+
+//exercise 3
+let a;
+let b;
+a = 1;
+b = 2;
+document.write (a, b);
+document.write (a=b);
+document.write (a, b)
+
+//exercise 4
+
+let bug = {
+     name: 'Lorem Ipsum', 
+     id: 7, 
+     reproducing: true, 
+     priority: ``,
+     severity: null
+    }  
+
+//exercise 5 (але тут можна ввести любе значення і всерівно виводиться меседж)
+
+let age = prompt('How old are you?');
+if (age >=18);
+console.log ("isAdult")
+
+
+//exercise 6
+
+let name = `Kate`;
+let surname = `Riaboshapko`;
+let YearOfBirth= 1993;
+let group = 522;
+let MaritalStatus = false;
+let education = null;
+let future;
+console.log ( typeof `Kate`);
+console.log ( typeof `Riaboshapko`);
+console.log (typeof 522);
+console.log (typeof false);
+console.log (typeof 1993);
+console.log (typeof future)
+
+//exercise 7 (тут би ще було б добре прописати інпут тайп для мила, але хз) 
+
+let login = prompt('Fill in your login' );
+alert(`Your  login is ${login}`);
+let email = prompt('Fill in your email address' );
+alert(`Your email address is ${email}`); 
+let password = prompt('Fill in your password');
+alert(`Your password is ${password}`)
+
+
+//exercise 8 (трохи рагульське рішення)
+
+let SecondsInHour = (60*60);
+let SecondsInDay = (60*60*24);
+let SecondsInMonth = (60*60*24*30);
+alert (SecondsInHour); 
+alert (SecondsInDay);
+alert (SecondsInMonth)
+
