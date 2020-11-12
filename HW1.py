8b294966bae8e263945ff79c2f6e66dc5599b919
catkate13 <57996108+catkate13@users.noreply.github.com>
2020-11-12 22:38:49 +0200

    Add files via upload

---
+++ HW1.py
@@ -0,0 +1,37 @@
+# 1163
+def watermelon(a):
+    result = 600-(a/6+a/6+27)
+    return result
+
+print(watermelon(600))
+
+# 13166
+
+def flax_seeds(a):
+    result = a/2
+    return result
+
+print(flax_seeds(244))
+
+# 1168
+
+def warm_days(a, b):
+    result = a*72/b
+    return result
+
+print(warm_days(9, 8))
+
+
+
+# 1172
+def dry_shypshyna(a):
+    result = a/2
+    return result
+
+print(dry_shypshyna(322))
+
+# 1169
+def corn(a):
+    result = a*2 - (a*2/10)
+    return result
+print(corn(300))
